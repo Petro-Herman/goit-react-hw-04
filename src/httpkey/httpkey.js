fb5391a15ae50b06979d4ea9d6c93d8dc4f7eace
Petro-Herman <germanpetr24@gmail.com>
2024-09-21 15:05:01 +0300

    cor2

--- src/httpkey/httpkey.js
+++ src/httpkey/httpkey.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 
-axios.defaults.baseURL =
-  "https://api.unsplash.com/search/photos/?client_id=rODOiE1Hh-ce3rtBQZmSmye6h_ClaLd_VW9K6UDQcnA";
+axios.defaults.baseURL = "https://api.unsplash.com/search/photos/";
 
 export const fetchPhoto = async (topic, page) => {
-  const response = await axios.get(
-    `https://api.unsplash.com/search/photos/?client_id=rODOiE1Hh-ce3rtBQZmSmye6h_ClaLd_VW9K6UDQcnA`
-  );
+  const response = await axios.get("", {
+    params: {
+      client_id: "rODOiE1Hh-ce3rtBQZmSmye6h_ClaLd_VW9K6UDQcnA",
+      query: topic,
+      page: page,
+    },
+  });
   return {
     photo: response.data.results,
     totalPages: response.data.total_pages,
